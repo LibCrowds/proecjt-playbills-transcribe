25c3118adbd43fd5361c70e15b89444616e46d91
Alexander Mendes <alexanderhmendes@gmail.com>
2017-04-18 08:24:16 +0100

    Refactor

--- generate_project.py
+++ generate_project.py
@@ -1,6 +1,6 @@
 #-*- coding: utf8 -*-
 """
-A script for generating the project-playbills-mark projects.
+A script for generating the project-playbills-transcribe projects.
 """
 import re
 import os
@@ -12,54 +12,53 @@ import argparse
 import shutil
 import itertools
 from BeautifulSoup import BeautifulSoup
+from jinja2 import Environment, FileSystemLoader
 
 
-def update_shortname(old_project_json, new_project_json):
-    """Update the short name in template and results files."""
-    here = os.path.dirname(__file__)
-    old = old_project_json['short_name']
-    new = new_project_json['short_name']
-    def replace(fn):
-        path_in = os.path.join(here, fn)
-        path_out = os.path.join(here, 'gen', fn)
-        with open(path_in, 'rb') as f:
-            data_in = f.read()
-        data_out = data_in.replace(old, new)
-        with open(path_out, 'wb') as f:
-            f.write(data_out)
-    replace('results.html')
-    replace('template.html')
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.dirname(__file__)),
+    trim_blocks=False
+)
 
 
-def copy_project_files():
-    """Copy template, tutorial, long_description and result files to gen."""
-    here = os.path.dirname(__file__)
-    def copy(fn):
-        shutil.copyfile(os.path.join(here, fn), os.path.join(here, 'gen', fn))
-    copy('long_description.md')
-    copy('template.html')
-    copy('tutorial.html')
-    copy('results.html')
+def verify_arks(data):
+    """Identify any arks that don't point to images via the BL IIIF api."""
+    for row in data:
+        ark = row['image_ark']
+        url = 'http://api.bl.uk/image/iiif/{}/info.json'.format(ark)
+        r = requests.get(url)
+        info = r.json()
+        if not info.get('tiles'):
+            raise ValueError('Bad Ark: {}'.format(ark))
 
 
-def write_project_json(rec_title, taskset):
-    """Write the project.json file."""
+def render_template(template_filename, context):
+    """Render a Jinja2 template."""
+    path = os.path.join(os.path.dirname(__file__), 'gen', template_filename)
+    tmpl = TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+    with open(path, 'wb') as f:
+        f.write(tmpl)
+
+
+def generate_project_context(rec_title, taskset):
+    """Write and return the project.json file."""
     prefix = 'A collection of playbills from '
     title = rec_title.replace(prefix, '').strip().rstrip('.').replace('"', '')
     name_suffix = taskset['nameSuffix']
     name = "{0}: {1}".format(title, name_suffix)
     invalidchars = r"([$#%·:,.~!¡?\"¿'=)(!&\/|]+)"
     shortname = re.sub(invalidchars, '', name.lower().strip()).replace(' ', '_')
-    project = {
+    context = {
         "name": name,
         "short_name": shortname,
         "description": taskset['description']
     }
     here = os.path.dirname(__file__)
-    path = os.path.join(here, 'gen', 'project.json')
+    path = os.path.join(os.path.dirname(__file__), 'gen', 'project.json')
     with open(path, 'wb') as f:
-        json.dump(project, f)
-    return project
+        json.dump(context, f)
+    return context
 
 
 def write_tasks_csv(fieldnames, data):
@@ -86,7 +85,8 @@ def get_json_data(json_data, taskset):
     
     product = list(itertools.product(tasks, input_data))
     data = [dict(row[0].items() + row[1].items()) for row in product]
-    for row in data:
+    input_rows = [r for r in data if r.get('inputs')]
+    for row in input_rows:
         row['inputs'] = json.dumps(row['inputs'])
     headers = set(itertools.chain(*[row.keys() for row in data]))
     return headers, data
@@ -105,6 +105,7 @@ def make_gen_dir():
         else:
             raise
 
+
 def get_record_title(aleph_sysno):
     """Attempt to retreive the title of the record from ALEPH."""
     base_url = "http://primocat.bl.uk/F/"
@@ -116,7 +117,7 @@ def get_record_title(aleph_sysno):
     for i, td in enumerate(rows):
         if td and 'Title &nbsp;' in td[0]:
             return rows[i + 1][0]
-    raise ValueError('No title for that system number could not be found')
+    raise ValueError('System number {0} could not be found'.format(aleph_sysno))
 
 
 def generate():
@@ -130,23 +131,24 @@ def generate():
     tasks_path = os.path.join(here, 'tasks.json')
     tasks_json = json.load(open(tasks_path, 'rb'))
     taskset = tasks_json[args.taskset]
-    project_json_path = os.path.join(here, 'project.json')
-    old_project_json = json.load(open(project_json_path, 'rb'))
 
     # Get the task data
     json_input = json.load(open(args.json, 'rb'))
     (headers, data) = get_json_data(json_input, taskset)
     sysno = data[1]['aleph_sys_no']
 
+    verify_arks(data)
     make_gen_dir()
     write_tasks_csv(headers, data)
     title = get_record_title(sysno)
-    new_project_json = write_project_json(title, taskset)
-    copy_project_files()
-    update_shortname(old_project_json, new_project_json)
+    context = generate_project_context(title, taskset)
+    
+    render_template('tutorial.html', context)
+    render_template('template.html', context)
+    render_template('results.html', context)
+    render_template('long_description.md', context)
     msg = '\n"{0}" created with {1} tasks'
-    print(msg.format(new_project_json['name'], len(data)))
-
+    print(msg.format(context['name'], len(data)))
 
 if __name__ == '__main__':
     generate()
